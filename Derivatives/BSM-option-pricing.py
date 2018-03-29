ab60722ad4a93f219114f40b67f988cebca4b746
Nicolas Borghol <nicolas_b@hotmail.fr>
2018-03-29 13:33:08 +0200

    update pricing methods

--- Derivatives/BSM-option-pricing.py
+++ Derivatives/BSM-option-pricing.py
@@ -1,6 +1,7 @@
 from scipy import stats
 import math
 import time
+import numpy as np
 
 def BSM(S, K, T, V, r, div, cp):
     """
@@ -14,15 +15,41 @@ def BSM(S, K, T, V, r, div, cp):
     """
     d1 = (math.log(S/K)+(r-div+0.5*math.pow(V,2))*T) / (V*math.sqrt(T))
     d2 = d1 - V*math.sqrt(T)
+    print(d1)
+    print(d2)
     
     optprice = cp * S * math.exp(-div*T)*stats.norm.cdf(cp*d1) - cp*K*math.exp(-r*T)*stats.norm.cdf(cp*d2)
-    return optprice
+    print("BS- The Option Price is: {0:.4f}".format(optprice))
 
 
+def Weiner(S,R,Vol,T,X):
+    return S * np.exp( (R- (Vol**2) / 2)*T + Vol*math.sqrt(T) * X)
 
+    
+def Monte_Carlo_Vanilla_Vectorization(S_0,K,R,Vol,T,cp, N_simu):
+    X = np.random.randn(N_simu)
+    assets_final_value = Weiner(S_0,R,Vol,T,X)
+    payoffs = np.maximum(cp*(assets_final_value-K), 0)
+ 
+    print("MC- Option Price is: {0:.4f}".format(np.mean(payoffs)*math.exp(-R*T)))
 
-tic = time.time()
-Opt = BSM(50,50,0.4167,0.4,0.1,0,-1)
-tac = time.time()
-print("The Option Price is: {0:.4f}".format(Opt))
-print("{:.8f}ms".format((tac-tic)*1000))
+def Monte_Carlo_Digital_Vectorization(S_0,K,R,Vol,T, cp, N_simu):
+    X = np.random.randn(N_simu)
+    assets_final_value = Weiner(S_0,R,Vol,T,X)
+    payoffs = np.where(cp*(assets_final_value-K) > 0, 1, 0)
+ 
+    print("MC- Digital Option Price is: {0:.4f}".format(np.mean(payoffs)*math.exp(-R*T)))
+
+S_0 = 50
+K = 50
+R = 0.1
+T = 0.4167
+Vol = 0.4
+div = 0
+cp = 1
+N_simu = 10000000
+
+BSM(S_0, K, T, Vol, R, div, cp)
+Monte_Carlo_Vanilla_Vectorization(S_0,K,R,Vol,T,cp, N_simu)
+Monte_Carlo_Digital_Vectorization(S_0,K,R,Vol,T,cp, N_simu)
+
--- Derivatives/option-pricing.py
+++ Derivatives/option-pricing.py
@@ -8,30 +8,92 @@ Created on Tue Mar 20 22:39:00 2018
 import time
 import math
 import numpy as np
+from scipy import stats
 
-class Option(object):
-    def __init__(self, S0, K, r, T, N, params):
+class EuropeanOption(object):
+    def __init__(self, S0, K, r, T, sigma, is_call):
         self.S0 = S0 # Stock index at time 0
         self.K = K # Strike price
         self.r = r # Riskfree rate
         self.T = T # Time maturity, in years
+        self.sigma = sigma # Volatility
+        self.is_call = is_call  # Call or put
+
+class BlackScholes(EuropeanOption):
+    def __init__(self, S0, K, r, T, sigma, div, is_call):
+        super().__init__(S0, K, r, T, sigma, is_call)
+        self.div = div
+
+    def __setup_parameters__(self):        
+        d1 = ( math.log(self.S0 / self.K) + (self.r - self.div +0.5*math.pow(self.sigma,2)) *self.T) \
+        / (self.sigma*math.sqrt(self.T))
+        d2 = d1 - self.sigma*math.sqrt(self.T)
+        self.Nd1 = stats.norm.cdf(self.is_call * d1)
+        self.Nd2 = stats.norm.cdf(self.is_call * d2)
+    
+    def value(self):
+        self.__setup_parameters__()
+        value = self.is_call * self.S0 * math.exp(-self.div * self.T)*self.Nd1 \
+                - self.is_call*self.K*math.exp(-self.r*self.T)*self.Nd2 
+        return value
+
+class MonteCarloVanilla(EuropeanOption):
+    def __init__(self, S0, K, r, T, sigma, div, is_call, niter):
+        super().__init__(S0, K, r, T, sigma, is_call)
+        self.niter = niter
+        
+    def _termvalues_(self):
+        """Vectorized Weiner Process"""
+        X = X = np.random.randn(self.niter)
+        return self.S0 * np.exp( (self.r- (self.sigma**2) / 2)*self.T \
+                                + self.sigma *math.sqrt(self.T) * X)
+
+    def _payoffs_(self):
+        terminal_prices = self._termvalues_()
+        return np.maximum(self.is_call*(terminal_prices-self.K), 0)
+    
+    def value(self):
+        return np.mean(self._payoffs_())*math.exp(-self.r*self.T)
+    
+    
+    
+class MonteCarloDigitale(MonteCarloVanilla):
+    def __init__(self, S0, K, r, T, sigma, div, is_call, niter):
+        super().__init__(S0, K, r, T, sigma, div, is_call, niter)
+        
+    def _payoffs_(self):
+        """Payoff of 1 if terminal price above K"""
+        terminal_prices = self._termvalues_()
+        return np.where(self.is_call*(terminal_prices-self.K) > 0, 1, 0) 
+        
+class MonteCarloAsian(MonteCarloVanilla):
+    def __init__(self, S0, K, r, T, sigma, div, is_call, niter, nsteps):
+        super().__init__(S0, K, r, T, sigma, div, is_call, niter)
+        self.nsteps = nsteps
+        self.STs = None
+    
+    def _termvalues_(self):
+        """Calculate the arithmetic average price of the paths"""
+        self.dt = self.T / self.nsteps
+        self.STs = np.full((self.niter, self.nsteps), self.S0, dtype=np.float32)
+        for i in range(self.nsteps):
+            X = np.random.randn(self.niter)
+            self.STs[:,i] = self.STs[:,i-1] * np.exp( (self.r- (self.sigma**2) / 2)*self.dt \
+                    + self.sigma*np.sqrt(self.dt) * X)
+        self.STs = self.STs.mean(axis=1)
+        return self.STs
+    
+class BinomCRR(EuropeanOption):
+    """Cox-Ross-Rubinstein European option valuation."""
+    def __init__(self, S0, K, r, T, sigma, is_call, N):
+        super().__init__(S0, K, r, T, sigma, is_call)
         self.N = max(1, N) # At least 1 period
         self.STs = None #Tree stock prices
         
-        """ Optional parameters used by derived classes """
-        self.sigma = params.get("sigma", 0)  # Volatility
-        self.is_call = params.get("is_call", True)  # Call or put
-        
-        """ Computed values """
-        self.dt = T/float(N)  # Single time step, in years
-        self.df = math.exp(-(r) * self.dt)  # Discount factor
-
-        
-class EuropeanCRR(Option):
-    """Cox-Ross-Rubinstein European option valuation."""
-    
     def __setup_parameters__(self):
         """ Required calculations for the model """
+        self.dt = self.T/float(self.N)  # Single time step, in years
+        self.df = math.exp(-(self.r) * self.dt)  # Discount factor
         self.M = self.N + 1  # Number of terminal nodes of tree
         self.u = math.exp(self.sigma * math.sqrt(self.dt)) # Up movement
         self.d = math.exp(-(self.sigma * math.sqrt(self.dt))) # Down movement
@@ -63,7 +125,7 @@ class EuropeanCRR(Option):
         payoffs = self._initialize_payoffs_tree_()
         return self._traverse_tree_(payoffs)
 
-    def price(self):
+    def value(self):
         """ The pricing implementation """
         self.__setup_parameters__()
         self._initialize_stock_price_tree_()
@@ -72,12 +134,17 @@ class EuropeanCRR(Option):
     
     def __str__(self):
         """Give description of the option when class is called"""
-        return "Call Option: {}\nPrice: {}\nStrike: {}\nSigma: {:.2f}%".format(self.is_call, self.S0, self.K, self.sigma*100)
+        return "Option: {}\nPrice: {}\nStrike: {}\nSigma: {:.2f}%".format(self.is_call, self.S0, self.K, self.sigma*100)
 
 
 
-eu_opt = EuropeanCRR(50, 50, 0.1, 0.4167, 10000, {"sigma":0.4, "is_call": False})
+#Option = BinomCRR(50, 50, 0.1, 0.4167, 0.4, 1, 10000)
+#Option = BlackScholes(50, 50, 0.1, 0.4167, 0.4, 0, 1)
+#Option = MonteCarloVanilla(50, 50, 0.1, 0.4167, 0.4, 0, 1, 100000)
+#Option = MonteCarloDigitale(50, 50, 0.1, 0.4167, 0.4, 0, 1, 100000)
+#Option = MonteCarloAsian(40, 40, 0.05, 0.5, 0.4, 0, 1, 10000, 1000)
+
 tic = time.time()
-print(eu_opt.price())
+#print(Option.value())
 tac = time.time()
 print("Exec time: {:.10f}s".format((tac-tic)))
